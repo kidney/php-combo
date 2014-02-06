fc30fb2543ccf56c8840aaa42f32b4611ea3bfd3
kidney <kidneyleung@gmail.com>
2014-02-06 22:49:13 +0800

    Update script_project.conf.php

--- config/script_project.conf.php
+++ config/script_project.conf.php
@@ -1,50 +1,24 @@
 <?php
 /**
- * combo¶¥¼¶±êÊ¶Â·¾¶ÅäÖÃ for javascript
+ * comboé¡¶çº§æ ‡è¯†è·¯å¾„é…ç½® for javascript
  */
 
 return array(
     'seajs' => array(
         'name' => 'seajs',
-        'path' => '/disk/data/htdocs233/poco_main/js_common/seajs'
+        'path' => 'examples/seajs'
     ),
     'utility' => array(
         'name' => 'utility',
-        'path' => '/disk/data/htdocs233/poco_main/js_common/utility'
+        'path' => 'examples/utility'
     ),
-    'matcha' => array(
-        'name' => 'matcha',
-        'path' => '/disk/data/htdocs233/poco_main/js_common/matcha'
+    'project1' => array(
+        'name' => 'project1',
+        'path' => 'examples/project1/assets/js'
     ),
-    'wo' => array(
-        'name' => 'wo',
-        'path' => '/disk/data/htdocs233/mypoco/qing/assets/js'
-    ),
-    'poco' => array(
-        'name' => 'poco',
-        'path' => '/disk/data/htdocs233/poco_main/js_common'
-    ),
-    'mobile_wo' => array(
-        'name' => 'mobile_wo',
-        'path' => '/disk/data/htdocs232/poco/new_mobile/mobile/js'
-    ),
-    'mpPublish' => array(
-        'name' => 'mpPublish',
-        'path' => '/disk/data/htdocs233/mypoco/blog_v2/js/publish'
-    ),
-    'm' => array(
-        'name' => 'm',
-        'path' => '/disk/data/htdocs233/mypoco/m/assets/js'
-    ),
-    'mtest' => array(
-        'name' => 'mtest',
-        'path' => '/disk/data/htdocs233/mypoco/m/test/assets/js'
-    ),
-
-    // ÉÌÒµ °¢Î°
-    'poco_app_photo' => array(
-        'name' => 'poco_app_photo',
-        'path' => '/disk/data/htdocs232/poco/topic/poco_app_photo/js'
+    'project2' => array(
+        'name' => 'project2',
+        'path' => 'examples/project2/assets/js'
     )
 );
 ?>
